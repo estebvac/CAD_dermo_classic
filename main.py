05db501974fb7c2ee8d8423bbbf2c888aa641ac6
Sergio814 <sergio.tascon@hotmail.com>
2019-10-18 18:18:29 +0200

    Issue #6: Add segmentation with level sets. Adding level set segmentation. Modifying other files so that both segmentation with watersheds and with level sets can be performed (one selected)

--- main.py
+++ main.py
@@ -3,5 +3,7 @@ from feature_extraction.training_data import prepate_datasets
 path_train = r"C:\Users\esteb\Documents\CAD_PROJECT\DERMO_TEST\train"
 path_val = r"C:\Users\esteb\Documents\CAD_PROJECT\DERMO_TEST\val"
 
-prepate_datasets(path_train, "train.csv")
-prepate_datasets(path_val, "val.csv")
+segmentation_alg = "ls" #"ws"
+
+prepate_datasets(path_train, "train.csv", segm_alg = segmentation_alg)
+prepate_datasets(path_val, "val.csv", segm_alg = segmentation_alg)
